fcd86f73b6e6e96731dd64196b72419dacd2b6f1
peter <1611641037@qq.com>
2022-12-09 23:04:15 +0800

    toast自定义

--- lib/system/review.dart
+++ lib/system/review.dart
@@ -1,7 +1,9 @@
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:utils/toast.dart';
 
-void review(String url) async {
+void launchUrl(String url) async {
   if (await launchUrlString(url)) {
     return;
   }
+  Toast.show(msg: '启动失败，请坚持是否安装该app');
 }
