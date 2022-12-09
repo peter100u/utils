63c395668f817db70dd99307faca23b1cdbc4ef9
peter <1611641037@qq.com>
2022-12-09 11:53:23 +0800

    评论review

--- lib/system/review.dart
+++ lib/system/review.dart
@@ -1,7 +1,6 @@
 import 'package:url_launcher/url_launcher_string.dart';
 
-void review(String appId) async {
-  var url = "https://apps.apple.com/app/id$appId?action=write-review";
+void review(String url) async {
   if (await launchUrlString(url)) {
     return;
   }
